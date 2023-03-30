d90ab56303749d14cc4580ddc6e4751b68e97078
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-30 16:57:39 +0530

    Add project files.

---
+++ ViewData/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index( int? id,string name,double? price)
+        {
+            ViewData["id"] = id;
+            ViewData["name"] = name;
+            ViewData["price"] = price;
+            return View();
+        }
+    }
+}
